9aa4c05d67e5b406cc1c53b8faeb34ba97aa07e8
Kayla34 <kalujan.sh@mysummitps.org>
2018-03-20 15:51:42 +0000

    first commit

--- int3checkpoints/info.js
+++ int3checkpoints/info.js
@@ -1,3 +1,16 @@
+//variables for each image 
 var piglet= document.getElementById("piglet");
 var pooh= document.getElementById("pooh");
+//the display div
 var display= document.getElementById("display");
+
+//piglet pic action item
+piglet.addEventListener("click", function(){
+    display.innerHTML="piglet is the classic cartoon sidekick of Pooh Bear."
+});
+
+//pooh pic action item
+pooh.addEventListener("click", function(){
+    display.innerHTML="pooh is the main character in The Adventures of Winnie the Pooh."
+    
+});
